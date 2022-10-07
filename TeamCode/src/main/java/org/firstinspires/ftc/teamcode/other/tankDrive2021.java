0436c0bfb967811bcac82982b700a5c824cbc00c
TheGreatOneNamedZach <thegreatonenamedzach@gmail.com>
2022-10-07 15:13:20 -0400

    Programmed test code for 1 motor.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/other/tankDrive2021.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/other/tankDrive2021.java
@@ -1,4 +1,25 @@
 package org.firstinspires.ftc.teamcode.other;
 
-public class tankDrive2021 {
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp(name="2021 Tank Drive", group="Other")
+public class tankDrive2021 extends OpMode{
+    private DcMotor motor1 = null;
+    private ElapsedTime runtime = new ElapsedTime();
+    double motorPower;
+
+    @Override
+    public void init() {
+        motor1 = hardwareMap.get(DcMotor.class, "motor1");
+    }
+
+    public void loop() {
+        telemetry.addData("Runtime", runtime);
+        motor1.setPower(motorPower);
+        telemetry.addData("Motor1", motorPower);
+    }
 }
