9e023864633be0e2b59d629a360ed1fd4c563076
EmraldXD <zekecrutch@gmail.com>
2022-10-14 16:17:01 -0400

    Zac's fixes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/motor/mechanicDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/motor/mechanicDrive.java
@@ -1,11 +1,13 @@
 package org.firstinspires.ftc.teamcode.motor;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import java.util.Arrays;
 
+@TeleOp(name = "Mechanum Drive", group = "e")
 public class mechanicDrive extends OpMode{
 
     DcMotor fR = null;
