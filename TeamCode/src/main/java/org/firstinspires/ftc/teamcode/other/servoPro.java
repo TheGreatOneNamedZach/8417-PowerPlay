13adfb5d18bc43b13d14ac56854b36f52649905a
TheGreatOneNamedZach <thegreatonenamedzach@gmail.com>
2022-11-18 16:38:46 -0500

    Fine tuning of the limit switch

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/other/servoPro.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.other;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name =  "Servo Programmer", group = "e")
+public class servoPro extends OpMode {
+    Servo claw;
+    @Override
+    public void init() {
+        claw = hardwareMap.get(Servo.class, "Claw");
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addData("Pos", claw.getPosition());
+        if(gamepad2.a) {
+            claw.setPosition(.57);
+        } else {
+            claw.setPosition(0.42);
+        }
+    }
+}
