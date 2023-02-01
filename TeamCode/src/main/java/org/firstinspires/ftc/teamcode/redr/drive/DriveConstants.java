9352fe4fa863cfc024eff3778618b61c696bd1e4
TheGreatOneNamedZach <thegreatonenamedzach@gmail.com>
2023-02-01 18:25:33 -0500

    Dumb RR bug ruined part of my night

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/redr/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/redr/drive/DriveConstants.java
@@ -32,7 +32,7 @@ public class DriveConstants {
      * from DriveVelocityPIDTuner.
      */
     public static final boolean RUN_USING_ENCODER = true;
-    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(0, 0, 0, 11.5885838);
+    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(0, 0, 1.5, 9.25);
 //getMotorVelocityF(MAX_RPM / 60 * TICKS_PER_REV)
     /*
      * These are physical constants that can be determined from your robot (including the track
@@ -43,7 +43,7 @@ public class DriveConstants {
      * convenience. Make sure to exclude any gear ratio included in MOTOR_CONFIG from GEAR_RATIO.
      */
     public static double WHEEL_RADIUS = 1.88976; // in
-    public static double GEAR_RATIO = 1.0; // output (wheel) speed / input (motor) speed
+    public static double GEAR_RATIO = 1.0; // output (wheel) speed / input (motor) speed 75.7232061548
     public static double TRACK_WIDTH = 11.5; // in
 
     /*
@@ -52,7 +52,7 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 1.0 / rpmToVelocity(MAX_RPM);
+    public static double kV = 1.0 / rpmToVelocity(MAX_RPM); // 0.016170174558485684
     public static double kA = 0;
     public static double kStatic = 0;
 
@@ -63,8 +63,8 @@ public class DriveConstants {
      * small and gradually increase them later after everything is working. All distance units are
      * inches.
      */
-    public static double MAX_VEL = 67.248417 * 0.80;
-    public static double MAX_ACCEL = 67.248417 * 0.80;
+    public static double MAX_VEL = 67.248417 * 0.60;
+    public static double MAX_ACCEL = 67.248417 * 0.60;
     public static double MAX_ANG_VEL = Math.toRadians(125.71278260869565);
     public static double MAX_ANG_ACCEL = Math.toRadians(125.71278260869565);
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/redr/drive/SampleMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/redr/drive/SampleMecanumDrive.java
@@ -273,24 +273,34 @@ public class SampleMecanumDrive extends MecanumDrive {
     @Override
     public List<Double> getWheelPositions() {
         List<Double> wheelPositions = new ArrayList<>();
+
+        wheelPositions.add(encoderTicksToInches(leftFront.getCurrentPosition() * -1));
+        wheelPositions.add(encoderTicksToInches(leftRear.getCurrentPosition()));
+
+        wheelPositions.add(encoderTicksToInches(rightFront.getCurrentPosition() * -1));
+        wheelPositions.add(encoderTicksToInches(rightRear.getCurrentPosition()));
+
+/*
         for (DcMotorEx motor : motors) {
             wheelPositions.add(encoderTicksToInches(motor.getCurrentPosition()));
-        }
+        }*/
         return wheelPositions;
     }
 
     @Override
     public List<Double> getWheelVelocities() {
         List<Double> wheelVelocities = new ArrayList<>();
+/*
         wheelVelocities.add(encoderTicksToInches(leftFront.getVelocity() * -1));
         wheelVelocities.add(encoderTicksToInches(leftRear.getVelocity()));
 
         wheelVelocities.add(encoderTicksToInches(rightFront.getVelocity() * -1));
         wheelVelocities.add(encoderTicksToInches(rightRear.getVelocity()));
-        /*
+*/
+
         for (DcMotorEx motor : motors) {
             wheelVelocities.add(encoderTicksToInches(motor.getVelocity()));
-        }*/
+        }
         return wheelVelocities;
     }
 
