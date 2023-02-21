4a9f5704790d09f28f89077f78155d9663cb3f41
EmraldXd <113649333+EmraldXd@users.noreply.github.com>
2023-02-21 18:14:59 -0500

    null backup for image detection works now; Image detection sees more accurately.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/initialize/autoLow.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/initialize/autoLow.java
@@ -214,7 +214,7 @@ public class autoLow extends LinearOpMode {
                 telemetry.addData("Rot", Math.toDegrees(drive.getLastError().getHeading()));
             } catch (Exception ignored) {}
 
-            if (Objects.equals(duck, "Robot") || Objects.equals(duck, "Light") || Objects.equals(duck, null)) {
+            if (Objects.equals(duck, "Robot") || Objects.equals(duck, "Light") || Objects.equals(duck, "not found.")) {
                 // Parks in the middle zone if the image is a robot or light bulb
                 // If no image is ever found, this will run
                 // If the driver does not wait for the image detector to load, this will run
